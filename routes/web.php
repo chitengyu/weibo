29ef76d074e2664261d7bde489b8924aea2abc8c
CHI <2433387@qq.com>
2019-01-22 06:11:52 +0000

    关注按钮

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,7 @@ Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy'
 //接着定义用户关注者列表和粉丝列表的路由，用于对接下来的关注人列表和粉丝列表进行显示。
 Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
 Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+
+//「关注用户」和「取消用户」的功能，加入路由定义
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
